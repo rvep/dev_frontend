92bc1d2182020e0b0adfcd34a3836a55ff8f7f3e
borysn <xborysn@gmail.com>
2016-09-03 13:10:23 -0500

    prevent going back to signin page after being authorized

--- src/rvep/signin/signin.component.ts
+++ src/rvep/signin/signin.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AuthService } from './shared';
 
 @Component({
-    selector: 'signin',
-    inputs: [],
-    templateUrl: 'signin.component.html',
-    styleUrls: ['signin.scss']
+  selector: 'signin',
+  inputs: [],
+  templateUrl: 'signin.component.html',
+  styleUrls: ['signin.scss']
 })
-export class Signin {}
+export class Signin implements OnInit {
+
+  constructor(private _authService:AuthService) {}
+
+  ngOnInit() {
+    // navigate if neccessary
+    this._authService.navigate();
+  }
+
+}
